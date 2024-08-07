8cd9f8d5bd751e9a6ff80875e1a66674ea138bb9
Christian Pechhacker <christian.pechhacker@realevalue.at>
2024-08-07 09:15:33 +0200

    unnötiges gelöscht

--- ShinyCP/app/map_data.R
+++ ShinyCP/app/map_data.R
@@ -4,28 +4,28 @@ library(tidyverse)
 
 leaflet_gruppen <- c("only selected", "all")
 
-shinyInput <- function(FUN, len, id, ...) {
-  inputs <- character(len)
-  for (i in seq_len(len)) {
-    inputs[i] <- as.character(FUN(paste0(id, i), label = NULL, ...))
-  }
-  inputs
-}
-
-js <- function(dtid, ns) {
-  c(
-    "$('[id^=checkb]').on('click', function(){",
-    "  var id = this.getAttribute('id');",
-    "  var i = parseInt(/checkb(\\d+)/.exec(id)[1]);",
-    "  var value = $(this).prop('checked');",
-    "  var info = [{row: i, col: 3, value: value}];",
-    sprintf(
-      "Shiny.setInputValue('%s', info);",
-      ns(sprintf("%s_cell_edit:DT.cellInfo", dtid))
-    ),
-    "})"
-  )
-}
+# shinyInput <- function(FUN, len, id, ...) {
+#   inputs <- character(len)
+#   for (i in seq_len(len)) {
+#     inputs[i] <- as.character(FUN(paste0(id, i), label = NULL, ...))
+#   }
+#   inputs
+# }
+#
+# js <- function(dtid, ns) {
+#   c(
+#     "$('[id^=checkb]').on('click', function(){",
+#     "  var id = this.getAttribute('id');",
+#     "  var i = parseInt(/checkb(\\d+)/.exec(id)[1]);",
+#     "  var value = $(this).prop('checked');",
+#     "  var info = [{row: i, col: 3, value: value}];",
+#     sprintf(
+#       "Shiny.setInputValue('%s', info);",
+#       ns(sprintf("%s_cell_edit:DT.cellInfo", dtid))
+#     ),
+#     "})"
+#   )
+# }
 
 
 
@@ -68,6 +68,7 @@ map_data_server <- function(id, data, ref_object){
 
       data %>% st_intersection(poly) %>% find_selected_objects(AnzahlVergleichsobjekte = input$n_vergleichsobjekte)
     })
+
 
     select_polygon <- reactive({
 
@@ -147,21 +148,21 @@ map_data_server <- function(id, data, ref_object){
       leafMap
     })
 
-    output$tmap <- renderTmap({
-      df_reference_object <- ref_object()
-      zsp_dat_small       <- zsp_dat %>% dplyr::filter(bez_id %in% df_reference_object$bez_id)
-      box                 <- df_reference_object %>% st_buffer(dist = 10000) %>% st_bbox()
-
-      raster_small <- ra_score_raster %>% st_crop(box)
-
-      df_reference_object %>%
-        tm_shape(bbox = box) +
-        tm_markers() +
-        tm_shape(raster_small) +
-        tm_raster(alpha = 0.6) +
-        tm_shape(zsp_dat_small) +
-        tm_borders()
-    })
+    # output$tmap <- renderTmap({
+    #   df_reference_object <- ref_object()
+    #   zsp_dat_small       <- zsp_dat %>% dplyr::filter(bez_id %in% df_reference_object$bez_id)
+    #   box                 <- df_reference_object %>% st_buffer(dist = 10000) %>% st_bbox()
+    #
+    #   raster_small <- ra_score_raster %>% st_crop(box)
+    #
+    #   df_reference_object %>%
+    #     tm_shape(bbox = box) +
+    #     tm_markers() +
+    #     tm_shape(raster_small) +
+    #     tm_raster(alpha = 0.6) +
+    #     tm_shape(zsp_dat_small) +
+    #     tm_borders()
+    # })
 
 
 
@@ -190,7 +191,6 @@ map_data_server <- function(id, data, ref_object){
 
       leafletProxy("map", data = df) %>%
         clearGroup(leaflet_gruppen) %>%
-        ## clearGroup("poly") %>%
         addCircleMarkers(df$xco_wgs84, df$yco_wgs84,
                          radius = ifelse(df$Selected == 0, 1, 10),
                          color  = ifelse(df$Selected == 0, "black", "red"),
@@ -200,84 +200,6 @@ map_data_server <- function(id, data, ref_object){
 
 
     })
-
-
-
-
-    # observe({
-    #   print("Zeichne Punkte neu!")
-    #   df <- data_selected()
-    #   df <- df %>% dplyr::mutate(Selected = if_else( row_number() %in% input$data_rows_selected, 0L, Selected))
-    #
-
-    #
-    #
-    #   leafletProxy("map", data = df) %>%
-    #     clearGroup(group = "immonet") %>%
-    #     leaflet::addCircleMarkers(
-    #       lng = df$xco_wgs84, lat = df$yco_wgs84,
-    #       group  = "immonet",
-    #       popup = point_popup,
-    #       color = if_else( df$Selected == 1, "red", "blue"),
-    #       stroke = FALSE, fillOpacity = 0.7
-    #     )
-    # })
-    #
-    # poly_drawn <- reactive({ ## eventReactive(input$map_draw_new_feature, {
-    #   feat   <- input$map_draw_new_feature
-    #   if(is.null(feat)) {
-    #     print("Generate empty polygon!")
-    #     empty_poly <- st_sf(st_sfc(), crs = 4326)
-    #     return(empty_poly)
-    #   }
-    #   if( ! is.null(input$map_draw_deletestop)) {
-    #     if(input$map_draw_deletestop & input$map_draw_new_feature$properties$`_leaflet_id` == input$map_draw_deleted_features$features[[1]]$properties$`_leaflet_id`) {
-    #       print("Feature wurde geloescht!")
-    #       empty_poly <- st_sf(st_sfc(), crs = 4326)
-    #       return(empty_poly)
-    #     }
-    #   }
-    #
-    #   coords <- unlist(feat$geometry$coordinates)
-    #   coords <- matrix(coords, ncol = 2, byrow = T)
-    #   poly   <- st_sf(st_sfc(st_polygon(list(coords))), crs = 4326)
-    #   poly
-    # })
-    #
-    # leaflet_id <- reactive({
-    #   if(is.null(input$map_draw_new_feature$properties$`_leaflet_id`) & ! is.null(polygon_data())) id <- 1 else id <- input$map_draw_new_feature$properties$`_leaflet_id`
-    #   print(paste("Leaflet ID:", id))
-    #   id
-    # })
-
-  #   data_selected <- eventReactive(leaflet_id(), { ## reactive({
-  #     print("Evaluliere Daten!")
-  #
-  #     poly           <- poly_drawn()
-  #     poly_all <<- poly
-  #     df     <- daten()
-  #
-  #     if( nrow(poly) == 0) {
-  #       print("Poly leer! Returniere alle Daten!")
-  #       df <- df %>% dplyr::mutate(Selected = 1L)
-  #     } else {
-  #       print("Verschneide mit Polygon!")
-  #       df <- df %>%
-  #         st_as_sf(coords = c("xco_wgs84", "yco_wgs84"), crs = 4326) %>%
-  #         st_intersection(poly) %>%
-  #         st_drop_geometry() %>%
-  #         dplyr::select(BewertungId) %>%
-  #         dplyr::mutate(Selected = 1L) %>%
-  #         dplyr::right_join(df, by = "BewertungId") %>%
-  #         dplyr::mutate(Selected = if_else(is.na(Selected), 0L, Selected))
-  #     }
-  #
-  #     df_all <<- df
-  #     df
-  #
-  #
-  #   })
-  #
 
 
     table_formatting <- reactive({
@@ -326,6 +248,7 @@ map_data_server <- function(id, data, ref_object){
       data_table_neu <<- editData(data_table_neu, info) %>%
         dplyr::mutate(PreisValorisiert = Kaufpreis * (1 + Zins)^Bezugszeitraum)
 
+      ## ToDO Update Table
       ## replaceData(proxy, data_table_neu, resetPaging = FALSE)  # important
     })
 
@@ -338,10 +261,12 @@ map_data_server <- function(id, data, ref_object){
 
       df %>%
         st_drop_geometry() %>%
-        dplyr::select(Kaufpreis, PreisValorisiert) %>%
+        dplyr::select(Kaufpreis, KaufpreisQM, PreisValorisiert) %>%
         ## dplyr::filter(Selected == 1) %>%
         dplyr::summarise_all(
-          c(Minimum = min, Mittelwert = function(x) mean(x) %>% round(-3), Maximum = max)
+          c(Minimum    = function(x) min(x, na.rm = T),
+            Mittelwert = function(x) mean(x, na.rm = T) %>% round(-3),
+            Maximum    = function(x) max(x, na.rm = T))
         ) %>%
         pivot_longer(everything()) %>%
         separate(name, into = c("Kaufpreis", "Statistik")) %>%
